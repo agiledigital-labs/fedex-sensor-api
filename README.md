a2766e3fc9d4de1800beaee5e5a1289763a6bad5
Sean Dawson <contact@seandawson.info>
2018-07-12 14:36:44 +1000

    Get basic query interface running

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fedex-sensor-api
 Fedex project interface to gather sensor data
+
+Example query:
+
+`/temperature?deviceName=K%20Temp&type=average&from=2018-07-12T02:03:19%2B00:00&to=2018-07-12T04:03:19%2B00:00`
--- routes/temperature.js
+++ routes/temperature.js
@@ -1,22 +1,79 @@
-var express = require('express');
-var router = express.Router()
-var influx = require('../db/influx');
+const express = require('express');
+const Influx = require('influx');
+const influx = require('../db/influx');
+const router = express.Router();
+
+function stringIsDefined(val) {
+  return typeof val === "string" && val.length > 0;
+}
+
+/*
+time  "2018-07-12T01:54:21.006Z"
+value 22.6
+*/
+
+const reducerFns = {
+  'average': function(seriesData) {
+    const values = seriesData.map((val) => val.value);
+    return values.reduce((prev, curr) => prev + curr, 0) / values.length;
+  },
+  'min': function(seriesData) {
+    const values = seriesData.map((val) => val.value);
+    return Math.min(...values);
+  },
+  'max': function(seriesData) {
+    const values = seriesData.map((val) => val.value);
+    return Math.max(...values);
+  },
+  'list': function(seriesData) {
+    return seriesData;
+  }
+};
+
+// TODO: Work out more elegant way to do this
+// See: https://node-influx.github.io/manual/usage.html#a-moment-for-times
+function toInfluxTime(isoDateString) {
+  console.log(isoDateString);
+  const timeValue = new Date(isoDateString).getTime();
+  const nanoTimeString = `${timeValue}000000`;
+  console.log(nanoTimeString);
+  return Influx.toNanoDate(nanoTimeString).toNanoISOString();
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	//time > now() - 1h 
-	//	AND 
 
-  influx.query(`
-	SELECT value
-	FROM "home_assistant"."autogen"."°C" 
-	WHERE "domain"='sensor'
-  `)
- 	.then(function(results) {
-  	  res.send(results);
-  	})
-  	.catch(err => {
-  	  console.dir(err);
+  const deviceName = req.query.deviceName;
+  const from = req.query.from;
+  const to = req.query.to;
+  const type = req.query.type;
+
+  const whereClauses = [`"domain"='sensor'`];
+
+  if (stringIsDefined(deviceName)) {
+    whereClauses.push(`"friendly_name_str"=${Influx.escape.stringLit(deviceName)}`);
+  }
+  if (stringIsDefined(from)) {
+    whereClauses.push(`time > '${toInfluxTime(from)}'`);
+  }
+  if (stringIsDefined(to)) {
+    whereClauses.push(`time < '${toInfluxTime(to)}'`);
+  }
+
+  const query = `
+    SELECT value
+    FROM "home_assistant"."autogen"."°C"
+    WHERE ${whereClauses.join(" AND ")}
+  `;
+  console.log(`query: '${query}'`);
+  influx.query(query, {precision: Influx.Precision.Seconds})
+  .then(function(results) {
+      const reducerFn = reducerFns[type] || reducerFns.list;
+      res.send({ 
+        result: reducerFn(results)
+      });
+    })
+    .catch(err => {
       next(err);
     })
 });
